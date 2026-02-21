949cb271408209c570f20222e465803bee709968
MUHAMMAD DAWOOD <113223960+idavidkhan@users.noreply.github.com>
2026-02-21 12:04:12 +0500

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,5 @@
+def call(String Project, String ImageTag, String dockerHubUser){
+  withCredentials([usernamePassword('credentialsId':"DockerHubCred", passwordVariable:"DockerHubPass", usernameVariable:"DockerHubUser")]){
+   sh "docker login -u ${env.DockerHubUser} -p ${env.DockerHubPass} "
+   sh "docker push ${env.DockerHubUser}/${Project}:${ImageTag}"
+}
